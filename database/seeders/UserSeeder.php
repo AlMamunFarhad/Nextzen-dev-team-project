7a0b0e3019ec449fdd9d825009952097a344e8d6
farhad <farhadmikky@gmail.com>
2026-02-07 17:15:49 +0600

    Multi authentication & Dashboard setup

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'role' => 0,
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Doctor',
+            'email' => 'doctor@gmail.com',
+            'role' => 1,
+            'password' => Hash::make('12345678'),
+        ]);
+        User::create([
+            'name' => 'Patient',
+            'email' => 'patient@gmail.com',
+            'role' => 2,
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
